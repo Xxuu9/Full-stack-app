1636fcabe4294c83384afdafc56a452e42053f27
Xxuu9 <tengxxu@gmail.com>
2024-03-10 15:55:09 -0400

    add error page and register page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,14 +1,22 @@
 import { useRouteError, Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import img from "../assets/images/not-found.svg";
 
 const Error = () => {
   const error = useRouteError();
-  console.log(error);
-  return (
-    <div>
-      <h1>Error Page</h1>
-      <Link to="/">Back home</Link>
-    </div>
-  );
+  // console.log(error);
+  if (error.status == 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Page not found!</h3>
+          <p>We can't find the page you are looking for</p>
+          <Link to="/dashboard">Back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
 };
 
 export default Error;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,34 @@
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to="/login">Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" defaultValue="Sophie" />
+        <FormRow
+          type="text"
+          name="lastName"
+          defaultValue="T"
+          labelText="last name"
+        />
+        <FormRow type="text" name="location" defaultValue="Canada" />
+        <FormRow type="email" name="email" defaultValue="Sophie@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="password000" />
+        <button type="submit" className="btn btn-submit">
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
